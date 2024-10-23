f256d0917c5b38fa284be259cf4a505b274c7220
jaykkoo <adrien.gutleben@gmail.com>
2024-10-23 14:45:31 +0200

    first commit

---
+++ scripts/after_install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+echo "Pull Finished"
+sudo systemctl daemon-reload
+sudo systemctl restart nginx
+#!/usr/bin/bash
+
+echo "Pull Finished"
+sudo systemctl daemon-reload
+sudo systemctl restart nginx
---
+++ scripts/before_install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# clean codedeploy-agent files for a fresh install
+sudo rm -rf /home/ubuntu/install
+
+# install CodeDeploy agent
+sudo apt update
+sudo apt install ruby
+sudo apt install wget
+
+cd /home/ubuntu
+wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+sudo chmod +x ./install 
+sudo ./install auto
+
+# update os & install python3
+sudo apt install -y python3 python3-dev python3-pip python3-venv
+pip install --user --upgrade virtualenv
+
+# delete app
+sudo rm -rf /home/ubuntu/django-aws_cicd
