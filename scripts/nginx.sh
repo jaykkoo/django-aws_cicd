9eb6b086e4cf8a3a9606aff8e0f07edbcd605f81
jaykkoo <adrien.gutleben@gmail.com>
2024-10-23 14:48:27 +0200

    change nginx

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -5,7 +5,7 @@ sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
 sudo cp /home/ubuntu/django-aws_cicd/nginx/nginx.conf /etc/nginx/sites-available/django-aws_cicd
-sudo ln -s /etc/nginx/sites-available/awscicd /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/django-aws_cicd /etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
 #sudo nginx -t
 sudo gpasswd -a www-data ubuntu
